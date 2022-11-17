6e30f2cb71ff185b60a4c6abba40dd9d2b2e2b24
Rodrigo-QM <116927501+Rodrigo-QM@users.noreply.github.com>
2022-11-17 10:18:17 -0400

    Add files via upload

---
+++ jquery con archivo/miseventos.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+    //Ejercicio 9
+    $(".oculta").click(function(){
+        $(this).hide();
+    });
+
+
+     //Ejercicio 10 
+     $(".doble").dblclick(function(){
+         $(this).hide();
+    });   
+});
